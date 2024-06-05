f68c3e8bc4cbcfd9c0d7f9110117f39dcc6866e4
Nusrat290920 <njs290920@gmail.com>
2024-06-05 12:15:49 +0600

    5 june  2024 classwork

---
+++ student.js
@@ -0,0 +1,11 @@
+const student_info = () => {
+    let name = "Nusrat";
+    let address = "Dhaka";
+    return `Name: ${name} <br> Address: ${address}`;
+}
+
+let country = "Japan";
+ let city = "Tokyo";
+
+export default student_info;
+export  { country, city };
